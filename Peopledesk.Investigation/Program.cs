5070a4f730b06fe5ae7020005913fc57ca376f29
Taosif Bin Murtaza <dev.taosif.net@gmail.com>
2025-02-03 00:33:33 +0600

    1608

--- Peopledesk.Investigation/Program.cs
+++ Peopledesk.Investigation/Program.cs
@@ -1,2 +1,30 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿namespace Peopledesk.Investigation;
+
+public class Solution
+{
+    public int SpecialArray(int[] nums)
+    {
+        var count = nums.Length;
+        for (var i = 0; i <= count; i++)
+        {
+            if (nums.Count(x => x >= i) == i )
+            {
+                return i;
+            }
+            
+        }
+        return -1;
+    }
+}
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Solution s = new();
+
+        int[] nums = [0,4,3,0,4]; 
+
+        Console.WriteLine(s.SpecialArray(nums));
+    }
+}
